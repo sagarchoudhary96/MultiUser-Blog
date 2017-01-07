a36b39aea71d8b86ca0be85312fa226db9f29c68
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-01-08 02:39:51 +0530

    Add: Client Side Signup validation

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,36 @@ $(".animi-shadow").hover(
 
     }
 );
+
+
+//checking validations for signup form
+var validate = function() {
+  var username = $("#username");
+  var email = $("#email");
+  var password = $("#password");
+  var verify_password = $("#verify_password");
+
+  //validation for null input
+  if (username.val() == "" || password.val()== "" || verify_password.val() == ""){
+    Materialize.toast("Required Fields can't be Empty", 5000);
+    return false;
+  }
+
+  //validation for username
+  var re = /^(([-\w\d]{1,10}))$/;
+  if (!(re.test(username.val()))){
+    Materialize.toast("Invalid Characters used", 5000);
+    username.focus();
+    return false;
+  }
+
+  //validation for email
+  re = /^(([-\w\d\.\@]{1,}))$/;
+  if (email.val()!="" && !(re.test(email.val()))){
+    Materialize.toast("Email Id contains invalid characters", 5000);
+    email.focus();
+    return false;
+  }
+
+  $("#signup").submit();
+};
