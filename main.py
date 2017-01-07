6fac680390c24218d79d6a8bc3c953c5d7532ae1
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-01-08 02:43:53 +0530

    update: signup template and main.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
 import os
+import re
+from google.appengine.ext import db
+from string import letters
 
 import jinja2
 import webapp2
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Posts(db.Model):
+     title = db.StringProperty(required = True)
+     imageurl = db.StringProperty(required = True)
+     content = db.TextProperty( required = True)
+     author = db.StringProperty(required = True)
+     created = db.DateTimeProperty(auto_now_add = True)
+     last_modified = db.DateTimeProperty(auto_now = True)
+     likes = db.IntegerProperty(required = True)
 
 
 class Handler(webapp2.RequestHandler):
@@ -20,12 +32,26 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+
+class Signup(Handler):
+    def get(self):
+        self.render("signup.html")
+
+    def post(self):
+        username = self.request.get("username")
+        email = self.request.get("email")
+        password = self.request.get("password")
+        verify_password = self.request.get("verify_password")
+        self.write("hello")
+
+
 class MainPage(Handler):
 
     def get(self):
-        self.write("Hello Udacity")
+        self.render("post.html")
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage)
+    ('/', MainPage),
+    ('/signup', Signup)
 ], debug=True)
